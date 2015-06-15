e621b22ab39853a5004f6d04bb22baa334a252cc
Nick White <britishnickk@gmail.com>
2015-06-15 05:21:36 -0500

    Items and prices can be added

--- src/compare.py
+++ src/compare.py
@@ -2,5 +2,22 @@
 
 #Need dictionary of items/prices, array of recipes
 
-#items[]=""
+items={}
 
+print "Please enter one item per line in the following format:"
+print "\"[item]\" [price]"
+print "Enter an empty line to finish \n"
+
+while(True):
+	itemPrice = raw_input("-->")
+	if itemPrice == "":
+		break
+	itemPrice= itemPrice.split('"')
+	if len(itemPrice) < 2 :
+		print "All items must have a price"
+	else:
+		item = itemPrice[1]
+		price = itemPrice[2]
+		items[item] = price
+
+		print ""+item+" costs "+items[item]
