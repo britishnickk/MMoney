0dd7d0543004b74f47ccb7a7c215802c5dbc6b20
Nicholas White <britishnickk@gmail.com>
2015-06-18 00:21:01 -0500

    Can now format raw recipe data to a form with only relevant info

--- GW2/formatRecipes.py
+++ GW2/formatRecipes.py
@@ -2,37 +2,36 @@
 import json
 import sys
 
-detail_file = open('recipe_details.json','r')
+#detail_file = open('recipe_details.json','r')
 #created_file = open('recipe_created_items.json','r')
 
 item_by_id=[]
 
 
 created_items={}	#dictionary made by the created items JSON file
-details = {} 	
+details = {}
 
-with open('recipe_created_items.json') as data_file:    
+with open('recipe_created_items.json') as data_file:
 	created_items = json.load(data_file)
 
 
-with open('recipe_details.json') as data_file:    
+with open('recipe_details.json') as data_file:
 	details = json.load(data_file)
 
-i=1
-while(True): 		#Loop over all recipes. We can't know how many there are beforehand
+for i in details.keys(): 		#Loop over all recipes. We can't know how many there are beforehand
 	recipe = details[str(i)]
-	i+=1
-	sys.stdout.write('"'+created_items[recipe['output_item_id']]['name']+'" '+recipe['output_item_count']+' "')
+	out = '"'+created_items[recipe['output_item_id']]['name']+'" '+recipe['output_item_count']+' "'
+	#sys.stdout.write('"'+created_items[recipe['output_item_id']]['name']+'" '+recipe['output_item_count']+' "')
 		#unfortunately, to be compatable with both python 2 and 3, to
 		#add more to this line, I need to use sys.stdout.write()
 	try:
 		for ingredient in recipe['ingredients']:
-			print ingredient['item_id']
-			print created_items[int(str(ingredient['item_id']))]
-			print ingredient['item_id']
-			sys.stdout.write( '"'+created_items[ingredient['item_id']]['name']+'" ')
-			sys.stdout.write( ingredient['count']+" ")
-		print ""	
+			out+= '"'+created_items[ingredient['item_id']]['name']+'" '
+			out+= ingredient['count']+" "
+			
+			#sys.stdout.write( '"'+created_items[ingredient['item_id']]['name']+'" ')
+			#sys.stdout.write( ingredient['count']+" ")
 	except KeyError:
 		continue
-
+	print out
+	
